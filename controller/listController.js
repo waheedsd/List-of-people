a531631e6870243a12f70b819663fdb9b83d074d
waheedsd <syedwaheed9292@gmail.com>
2019-09-21 08:48:09 +0530

    Updated VIEW API

--- controller/listController.js
+++ controller/listController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
+const List = mongoose.model('List');
 router.get('/',(req,res)=>{
     res.render("list/addoredit",{
         viewtitle : "List of people"
@@ -7,6 +9,45 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/',(req,res)=>{
-    console.log(req);
+    insertRecord(req,res);
 })
+function insertRecord(req,res){ 
+    var list = new List();
+    list.name = req.body.Name;
+    list.age = req.body.Age;
+    list.gender = req.body.Gender;
+    list.mobileNumber = req.body.mobileNumber;
+    list.save((err,doc)=>{
+        if(!err){
+            res.redirect('list/show');
+        }
+        else{
+            console.log("Error during insertion"+err);
+        }
+    })
+}
+router.get('/details',(req,res )=>{
+    List.find((err,docs)=>{
+        if(!err){
+            res.render("list/details",{
+                details:docs
+            });
+            return;
+        }
+        else{
+            console.log("Error in rendering List details"+err);
+        }
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    List.findById(req.params.id,(err,doc)=>{
+        if(!err){
+            res.render("list/addoredit",{
+                viewtitle:"update list",
+                listdetails:doc
+            })
+        }
+    });
+});
 module.exports = router;
